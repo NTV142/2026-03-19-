0af80c2e057fe7266109e06ba04d04f1bd82f146
NTV142 <nakazato19480704@gmail.com>
2026-03-19 12:06:48 +0900

    Delete 0319js/js_kuramae directory

--- 0319js/js_kuramae/main14.js
+++
@@ -1,7 +0,0 @@
-﻿$(function(){
-  $("#left,#right").on("click", openDoor);
-});
-function openDoor() {
-  $("#left").animate({"left" : "-200px"}, 400);
-  $("#right").animate({"left" : "400px"}, 400);
-}
